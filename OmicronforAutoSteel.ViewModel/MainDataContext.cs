aad950977f71016cbf0d269fdccf7bb208ee7ee3
sunxinjian <xinjian_sun@sina.com>
2017-08-03 15:32:00 +0800

    mvvm ready

---
+++ OmicronforAutoSteel.ViewModel/MainDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BingLibrary.hjb;
+using BingLibrary.hjb.Intercepts;
+using System.ComponentModel.Composition;
+
+namespace OmicronforAutoSteel.ViewModel
+{
+    [BingAutoNotify]
+    public class MainDataContext : DataSource
+    {
+        public virtual string Text { set; get; } = "123";
+    }
+    class VMManager
+    {
+        [Export(MEF.Contracts.Data)]
+        [ExportMetadata(MEF.Key, "md")]
+        MainDataContext md = MainDataContext.New<MainDataContext>();
+    }
+}
